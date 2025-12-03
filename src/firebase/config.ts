2af27f34545e567c9770ebc695fa6ed5b0b28838
Matt Hager <MattHagerSr@gmail.com>
2025-12-03 15:13:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1372065081-a4a38",
+  "appId": "1:658748845776:web:309ae1112e0da5d2ae78d5",
+  "apiKey": "AIzaSyDXB04wDSGMIz5TK4VZeiE_OyLGdhKQNaA",
+  "authDomain": "studio-1372065081-a4a38.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "658748845776"
+};
